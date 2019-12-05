b53f834446de601ecc98d1a5bf410144d2888d6a
Jack Wallace <jack.wallace@zenuity.com>
2019-12-05 15:36:54 -0500

    Added initial database queries

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCVRxXm5EXKYF0SWLUlZNGNINBqgJm59wk",
+    authDomain: "zpotizpiez.firebaseapp.com",
+    databaseURL: "https://zpotizpiez.firebaseio.com",
+    projectId: "zpotizpiez",
+    storageBucket: "zpotizpiez.appspot.com",
+    messagingSenderId: "146222850402",
+    appId: "1:146222850402:web:4d813e574954c093c84c3b",
+    measurementId: "G-CEN81PQXRX"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+export {db};
