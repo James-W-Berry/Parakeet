66ff819439192d1b80a43fe824c0c6b889149eae
James-W-Berry <james.berry.iv@gmail.com>
2019-12-05 16:43:22 -0500

    add splash screen component

---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import SavedList from "../SavedList/SavedList";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <SavedList />
+      </div>
+    );
+  }
+}
+
+export default Main;
