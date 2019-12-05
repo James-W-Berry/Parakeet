359ce4653419f94d9068221fbfb78ea538d7301c
Jack Wallace <JackCWallace51@gmail.com>
2019-12-05 15:36:54 -0500

    Added initial database queries

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './images/logo.png';
 import './App.css';
+import SavedList from "./SavedList";
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SpotiSpiez 4 eva
+          <SavedList />
         </p>
         <a
           className="App-link"
---
+++ src/SavedList.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { db } from "./firebase";
+
+class SavedList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    db.collection("users")
+      .get()
+      .then(querySnapshot => {
+        const data = querySnapshot.docs.map(doc => doc.data());
+        console.log(data);
+        this.setState({ users: data });
+      });
+  }
+
+  render() {
+    const { users } = this.state;
+    
+    return(
+      <div >
+        {users.map(user => (
+          <div>
+            <div> {user.firstName} {user.lastName} </div>
+            <div> {user.currentSong} </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default SavedList;
