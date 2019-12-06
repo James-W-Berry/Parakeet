22b81374c9bd12fc60d799304d89e70e30be3cb8
James-W-Berry <james.berry.iv@gmail.com>
2019-12-06 00:22:44 -0500

    update UI components with Firebase integration

--- src/components/TrendingList/TrendingList.js
+++ src/components/TrendingList/TrendingList.js
@@ -1,24 +1,53 @@
 import React, { Component } from "react";
-import WhatsTrendingController from "../WhatsTrendingController/WhatsTrendingController";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import audio_wave from "../../images/audio_wave.png";
+import uuid from "react-uuid";
 
 class TrendingList extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       songList: [],
-      timerange: 6.048e+8
+      timerange: 6.048e8
     };
   }
 
+  createSongItem(song) {
+    console.log(`creating a playlist entry from ${song}`);
+    if (song !== undefined) {
+      return (
+        <ListItem key={uuid()}>
+          <ListItemAvatar>
+            <Avatar>
+              <img src={audio_wave} alt="" />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            primary={song.songId}
+            secondary={`${song.currentSong} * ${song.lastName}`}
+          />
+        </ListItem>
+      );
+    }
+  }
+
   render() {
-  var trendingData = <WhatsTrendingController />;
-    return (
-      <div>
-      <br/><br/>
-      {trendingData}
-      <br/><br/>
-      </div>
-    );
+    // const classes = useStyles();
+    console.log(this.props.pastSongs);
+
+    if (this.props.pastSongs !== undefined) {
+      return (
+        <List>
+          {this.props.pastSongs.map(song => this.createSongItem(song))}
+        </List>
+      );
+    } else {
+      return null;
+    }
   }
 }
 
