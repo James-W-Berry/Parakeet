f141ab606f4205999723b0119beff48ec2b79fa8
James-W-Berry <james.berry.iv@gmail.com>
2019-12-06 04:36:09 -0500

    add spotify component with auth server

--- src/components/SpotifyPlayer/SpotifyPlayer.js
+++ src/components/SpotifyPlayer/SpotifyPlayer.js
@@ -1,6 +1,67 @@
 import React, { Component } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotifyApi = new SpotifyWebApi();
 
 class SpotifyPlayer extends Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      nowPlaying: { name: "Not Checked", albumArt: "" },
+      playing: true
+    };
+  }
+
+  getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q);
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  getNowPlaying() {
+    spotifyApi.getMyCurrentPlaybackState().then(response => {
+      this.setState({
+        nowPlaying: {
+          name: response.item.name,
+          albumArt: response.item.album.images[0].url
+        }
+      });
+    });
+  }
+
+  postNextSong() {
+    spotifyApi.skipToNext();
+    this.getNowPlaying();
+  }
+
+  postPreviousSong() {
+    spotifyApi.skipToPrevious();
+    this.getNowPlaying();
+  }
+
+  postPlayPause() {
+    if (this.playing) {
+      spotifyApi.pause();
+      this.playing = false;
+    } else {
+      spotifyApi.play();
+      this.playing = true;
+    }
+  }
+
   render() {
     return (
       <div
@@ -12,7 +73,32 @@ class SpotifyPlayer extends Component {
           borderTopLeftRadius: "120px"
         }}
       >
-        I'm the Spotify Player Component
+        <div className="App">
+          <a href="https://spotispies-spotifyauthserver.firebaseapp.com:8888/login">
+            {" "}
+            Login to Spotify{" "}
+          </a>
+          <div>Now Playing: {this.state.nowPlaying.name}</div>
+          <div>
+            <img src={this.state.nowPlaying.albumArt} style={{ height: 150 }} />
+          </div>
+          <div>
+            {this.state.loggedIn && (
+              <button onClick={() => this.getNowPlaying()}>
+                Check Now Playing
+              </button>
+            )}
+          </div>
+          {this.state.loggedIn && (
+            <button onClick={() => this.postPreviousSong()}>Prev Song</button>
+          )}
+          {this.state.loggedIn && (
+            <button onClick={() => this.postPlayPause()}>Pause/Play</button>
+          )}
+          {this.state.loggedIn && (
+            <button onClick={() => this.postNextSong()}>Next Song -></button>
+          )}
+        </div>
       </div>
     );
   }
