7d6d6abb70b5263fc315bfbaaa36e69619d4d221
James-W-Berry <james.berry.iv@gmail.com>
2020-01-26 15:38:48 -0500

    redesign splash screen and login sequence

--- src/components/SplashScreen/SplashScreen.js
+++ src/components/SplashScreen/SplashScreen.js
@@ -4,47 +4,115 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./SplashScreen.css";
 import Main from "../Main/Main.js";
 import bannerLogo from "../../images/bannerLogo.png";
+import SpotifyIcon from "./SpotifyIcon";
+import {
+  setToken,
+  setUser,
+  setLocation,
+  setCurrentSong
+} from "../../actions/actions";
+import { connect } from "react-redux";
+import { Button } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const loginUrl = process.env.REACT_APP_LOGIN_URL;
+const devLoginUrl = process.env.REACT_APP_DEV_LOGIN_URL;
+
+const StyledButton = withStyles({
+  root: {
+    background: "#1db954",
+    borderColor: "#1aa34a",
+    borderRadius: 40,
+    border: 0,
+    color: "white",
+    height: "10vh",
+    width: "20vw",
+    padding: "0 30px",
+    boxShadow: "0 3px 5px 2px rgba(29, 185,	84, .3)"
+  },
+  label: {
+    textTransform: "capitalize"
+  }
+})(Button);
 
 class SplashScreen extends Component {
   constructor(props) {
     super(props);
+    const params = this.getHashParams();
+    const token = params.access_token;
+
+    if (token) {
+      this.props.setToken(token);
+    }
+
     this.state = {
+      loggedIn: token ? true : false,
+      token: token,
       done: undefined
     };
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(response => response.json())
-        .then(json => this.setState({ done: true }));
-    }, 2000);
+  getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q);
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
   }
 
   render() {
     return (
-      <div>
-        {!this.state.done ? (
+      <div className="SplashScreen">
+        {!this.state.loggedIn && (
           <FadeIn>
             <div
-              className="SplashScreen"
               style={{
                 display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center"
+                flex: 2,
+                flexDirection: "row",
+                justifyContent: "center"
               }}
             >
               <h1 className="SplashScreen_text">Parakeet</h1>
-              <img src={bannerLogo} alt="" height="500" width="390" />
+              <img src={bannerLogo} alt="" height="300" width="234" />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                marginTop: "20vh",
+                justifyContent: "center",
+                alignSelf: "center"
+              }}
+            >
+              <StyledButton
+                variant="contained"
+                href={loginUrl}
+                startIcon={<SpotifyIcon />}
+              >
+                Login with Spotify
+              </StyledButton>
             </div>
           </FadeIn>
-        ) : (
-          <Main />
         )}
+        {this.state.loggedIn && <Main />}
       </div>
     );
   }
 }
 
-export default SplashScreen;
+const mapDispatchToProps = {
+  setToken: setToken,
+  setUser: setUser,
+  setLocation: setLocation,
+  setCurrentSong: setCurrentSong
+};
+
+export default connect(null, mapDispatchToProps)(SplashScreen);
---
+++ src/components/SplashScreen/SpotifyIcon.js
@@ -0,0 +1,7 @@
+import React from "react";
+import spotify from "../../images/spotify_black.png";
+
+const SpotifyIcon = () => {
+  return <img src={spotify} width={60} height={60} alt="Login" />;
+};
+export default SpotifyIcon;
